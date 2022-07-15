20a665518e636c740ade98bed8018e6d56f257ec
muskan chourasia <mchourasia@technomatz.com>
2022-07-15 11:08:58 +0530

    1st commit

---
+++ storage.js
@@ -0,0 +1,26 @@
+function saveData()
+{
+let name,email,number,password;
+name = document.getElementById("name").value;
+email = document.getElementById("email").value;
+number = document.getElementById("number").value;
+password = document.getElementById("password").value;
+
+let user_records=new Array()
+user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+if(user_records.some((v)=>{return v.email==email}))
+{
+    alert("duplicate data");
+}
+else
+{
+    user_records.push({
+    "name":name,
+    "email":email,
+    "number":number,
+    "password":password,
+    })
+    localStorage.setItem("user",JSON.stringify(user_records));
+    
+}
+}
